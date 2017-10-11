c762271877b185293eded201bcbb3df8d6db11cd
Peter Zijlstra <peterz@infradead.org>
2017-10-11 15:52:32 +0200

    mm: RCU free VMAs
    Manage the VMAs with SRCU such that we can do a lockless VMA lookup.
    
    We put the fput(vma->vm_file) in the SRCU callback, this keeps files
    valid during speculative faults, this is possible due to the delayed
    fput work by Al Viro -- do we need srcu_barrier() in unmount
    someplace?
    
    We guard the mm_rb tree with a seqlock (this could be a seqcount but
    we'd have to disable preemption around the write side in order to make
    the retry loop in __read_seqcount_begin() work) such that we can know
    if the rb tree walk was correct. We cannot trust the restult of a
    lockless tree walk in the face of concurrent tree rotations; although
    we can trust on the termination of such walks -- tree rotations
    guarantee the end result is a tree again after all.
    
    Furthermore, we rely on the WMB implied by the
    write_seqlock/count_begin() to separate the VMA initialization and the
    publishing stores, analogous to the RELEASE in rcu_assign_pointer().
    We also rely on the RMB from read_seqretry() to separate the vma load
    from further loads like the smp_read_barrier_depends() in regular
    RCU.
    
    We must not touch the vmacache while doing SRCU lookups as that is not
    properly serialized against changes. We update gap information after
    publishing the VMA, but A) we don't use that and B) the seqlock
    read side would fix that anyhow.
    
    We clear vma->vm_rb for nodes removed from the vma tree such that we
    can easily detect such 'dead' nodes, we rely on the WMB from
    write_sequnlock() to separate the tree removal and clearing the node.
    
    Provide find_vma_srcu() which wraps the required magic.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    
    [Remove the warnings in description about the SRCU global lock which
     has been removed now]
    [Rename vma_is_dead() to vma_has_changed() and move its adding to the next
     patch]
    [Postpone call to mpol_put() as the policy can be used during the
     speculative path]
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    
    mm/spf: Fix policy free

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -326,6 +326,7 @@ struct vm_area_struct {
 #ifdef CONFIG_SPF
 	seqcount_t vm_sequence;
 #endif
+	struct rcu_head vm_rcu_head;
 } __randomize_layout;
 
 struct core_thread {
@@ -344,6 +345,7 @@ struct mm_struct {
 	struct {
 		struct vm_area_struct *mmap;		/* list of VMAs */
 		struct rb_root mm_rb;
+		seqlock_t mm_seq;
 		u64 vmacache_seqnum;                   /* per-thread vmacache */
 #ifdef CONFIG_MMU
 		unsigned long (*get_unmapped_area) (struct file *filp,
