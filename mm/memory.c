810d8607126890efac947e7ba4672133f8dd3d26
Peter Zijlstra <peterz@infradead.org>
2017-10-11 15:52:27 +0200

    mm: Dont assume page-table invariance during faults
    One of the side effects of speculating on faults (without holding
    mmap_sem) is that we can race with free_pgtables() and therefore we
    cannot assume the page-tables will stick around.
    
    Remove the reliance on the pte pointer.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    
    [Remove only if !CONFIG_SPF]
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2305,6 +2305,7 @@ int apply_to_page_range(struct mm_struct *mm, unsigned long addr,
 }
 EXPORT_SYMBOL_GPL(apply_to_page_range);
 
+#ifndef CONFIG_SPF
 /*
  * handle_pte_fault chooses page fault handler according to an entry which was
  * read non-atomically.  Before making any commitment, on those architectures
@@ -2314,7 +2315,7 @@ EXPORT_SYMBOL_GPL(apply_to_page_range);
  * and do_anonymous_page can safely check later on).
  */
 static inline int pte_unmap_same(struct mm_struct *mm, pmd_t *pmd,
-				pte_t *page_table, pte_t orig_pte)
+				 pte_t *page_table, pte_t orig_pte)
 {
 	int same = 1;
 #if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT)
@@ -2328,6 +2329,7 @@ static inline int pte_unmap_same(struct mm_struct *mm, pmd_t *pmd,
 	pte_unmap(page_table);
 	return same;
 }
+#endif /* CONFIG_SPF */
 
 static inline void cow_user_page(struct page *dst, struct page *src, unsigned long va, struct vm_area_struct *vma)
 {
@@ -2908,8 +2910,13 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 	int exclusive = 0;
 	vm_fault_t ret = 0;
 
-	if (!pte_unmap_same(vma->vm_mm, vmf->pmd, vmf->pte, vmf->orig_pte))
+#ifndef CONFIG_SPF
+	if (!pte_unmap_same(vma->vm_mm, vmf->pmd, vmf->pte, vmf->orig_pte)) {
+		if (page)
+			put_page(page);
 		goto out;
+	}
+#endif
 
 	entry = pte_to_swp_entry(vmf->orig_pte);
 	if (unlikely(non_swap_entry(entry))) {
